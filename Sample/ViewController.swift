47e3c638029eb10cd33be2ee0dabf5229efaa686
Ahd Radwan <radwanahd@gmail.com>
2020-04-28 04:31:52 +0300

    Implement demo view controllers

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func didClickInsertData(_ sender: Any) {
+//                let palestine = CountryModel.init();
+//                palestine.name = "Palestine";
+//                palestine.area = 27000;
+//                palestine.code = "PS";
+//                palestine.callingCode = "+972"
+//                palestine.id = "0"
+//                palestine.population = 5051953
+//                palestine.save();
+//
+//
+//                let lebanon = CountryModel.init();
+//                lebanon.name = "Lebanon";
+//                lebanon.area = 10452;
+//                lebanon.code = "LB";
+//                lebanon.callingCode = "+972"
+//                lebanon.population = 10452;
+//                lebanon.id = "1";
+//                lebanon.save()
+//
+//                let ramallah = CityModel.init();
+//                ramallah.name = "Rmallah";
+//                ramallah.population = 27092;
+//                ramallah.isCapital = false;
+//                ramallah.country = palestine
+//                ramallah.id = "0"
+//                ramallah.save();
+//
+//                let jerusalem = CityModel.init();
+//                jerusalem.name = "Jerusalem";
+//                jerusalem.population = 27092;
+//                jerusalem.isCapital = true;
+//                jerusalem.country = palestine
+//                jerusalem.id = "1"
+//                jerusalem.save();
+//
+//                let beirut = CityModel.init();
+//                beirut.name = "Beirut";
+//                beirut.population = 361366;
+//                beirut.isCapital = true;
+//                beirut.country = lebanon
+//                beirut.id = "2"
+//                beirut.save();
+//
+//                let tyre = CityModel.init();
+//                tyre.name = "Tyre";
+//                tyre.population = 60000;
+//                tyre.isCapital = false;
+//                tyre.country = lebanon
+//                tyre.id = "3"
+//                tyre.save();
 
+    }
+    
 }
 
